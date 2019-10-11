0e2a7f80d220b5595c4844c62870049d7273e7f0
Zaim Imran <zaim.imran@gmail.com>
2019-10-11 15:14:06 +0200

    able to pick nr of cakes

--- src/App/App.js
+++ src/App/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/styles';
 import clsx from 'clsx';
 
-import { Card, CardActions, CardContent, Typography, TextField, InputAdornment } from '@material-ui/core';
+import { Card, CardContent, Typography, TextField, InputAdornment, FilledInput } from '@material-ui/core';
 
 const styles = (theme) => ({
   card: {
     width: '40vw',
     minWidth: '500px',
     position: 'absolute',
-    top: '30%',
+    top: '40%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
   },
@@ -35,6 +35,9 @@ const styles = (theme) => ({
     flexBasis: 200,
     margin: 0,
   },
+  cake: {
+    width: '35px',
+  }
 });
 
 class App extends Component {
@@ -42,14 +45,29 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 0
+      value: 0,
+      box: 0,
+      cake: 0,
     }
   }
 
   handleChange(e) {
     this.setState({ value: e.target.value })
-    console.log(this.state.value);
+    this.calculate(e)
+  }
+  handleChangeCake(e) {
+    if(e.target.value < 0){
+      this.setState({ cake: 0 })
+      this.setState({ box: this.state.value})
+    }else if(this.state.value - e.target.value*3>=0){
+      this.setState({ cake: e.target.value })
+      this.setState({ box: this.state.value - e.target.value*3 })
+    }
+  }
 
+  calculate(e) {
+    this.setState({cake: Math.floor(e.target.value/3)});
+    this.setState({box: e.target.value % 3});
   }
 
   render() {
@@ -66,7 +84,7 @@ class App extends Component {
             {bull}
             eim
             {bull}
-            s
+            es
             {bull}
             kon
             {bull}
@@ -77,7 +95,7 @@ class App extends Component {
           </Typography>
           <br />
           <TextField
-            id="units"
+            id="Units"
             className={clsx(classes.margin, classes.textField)}
             variant="outlined"
             label="Amount"
@@ -91,10 +109,28 @@ class App extends Component {
             }}
           />
           {this.state.value !== 0 &&
-            <Typography>
-              <br />
-              he
-            </Typography>
+            <div>
+              <Typography>
+                <br />
+                {this.state.value} 0.33l Boks
+              </Typography>
+              <hr />
+              <Typography>
+                {this.state.box + " 0.33l Boks"}
+                <br />
+                <TextField
+                  id="Cake"
+                  value={this.state.cake}
+                  onChange={(e) => this.handleChangeCake(e)}
+                  type="number"
+                  className={clsx(classes.textField, classes.dense, classes.cake)}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                {" Toro kake"}
+              </Typography>
+            </div>
           }
         </CardContent>
       </Card>
